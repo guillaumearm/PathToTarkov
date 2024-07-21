b8bf6efe9874a0b322d131cb41e11476c768cbb8
Trap <contact@trapcodien.com>
2024-07-21 06:07:37 +0200

    refactor: uninstall procedure

--- src/uninstall.ts
+++ src/uninstall.ts
@@ -5,70 +5,75 @@ import { JAEGER_ID } from "./config";
 import { isJaegerIntroQuestCompleted } from "./helpers";
 import { getMainStashId } from "./utils";
 
+const restoreMainStash = (profile: Profile, logger: ILogger): void => {
+  const pmcInventory = profile.characters.pmc.Inventory;
+
+  const mainStashId = getMainStashId(profile);
+
+  if (mainStashId !== pmcInventory.stash) {
+    logger.success(
+      `=> PathToTarkov: restore the selected stash to main stash for profile '${profile.info.username}'`
+    );
+    pmcInventory.stash = mainStashId;
+  }
+};
+
+const restoreTraders = (config: Config, profile: Profile, logger: ILogger) => {
+  let nbTradersRestored = 0;
+  let jaegerLocked = false;
+
+  Object.keys(config.traders_config).forEach((traderId) => {
+    const pmc = profile.characters.pmc;
+    const trader = pmc.TradersInfo?.[traderId];
+    const jaegerAvailable = isJaegerIntroQuestCompleted(pmc.Quests);
+
+    if (
+      trader &&
+      trader.unlocked === false &&
+      ((traderId === JAEGER_ID && jaegerAvailable) || traderId !== JAEGER_ID)
+    ) {
+      trader.unlocked = true;
+      nbTradersRestored += 1;
+    } else if (
+      trader &&
+      trader.unlocked === true &&
+      traderId === JAEGER_ID &&
+      !jaegerAvailable
+    ) {
+      trader.unlocked = false;
+      jaegerLocked = true;
+    }
+  });
+
+  if (nbTradersRestored > 0) {
+    logger.success(
+      `=> PathToTarkov: ${nbTradersRestored} trader${
+        nbTradersRestored === 1 ? "" : "s"
+      } restored for profile '${profile.info.username}'`
+    );
+  }
+
+  if (jaegerLocked) {
+    logger.success(
+      `=> PathToTarkov: Jaeger trader locked (because introduction quest is not completed) for profile '${profile.info.username}'`
+    );
+  }
+};
+
 // Used for uninstallation process
 export const purgeProfiles = (
   config: Config,
   saveServer: SaveServer,
   logger: ILogger
 ): void => {
-  // be sure to be able to read `SaveServer.profiles`
+  // because we want to be sure to be able to read `SaveServer.profiles`
   saveServer.load();
 
-  const profiles = saveServer.getProfiles();
-
-  Object.keys(profiles).forEach((sessionId) => {
+  Object.keys(saveServer.getProfiles()).forEach((sessionId) => {
     const profile: Profile = saveServer.getProfile(sessionId);
-    const pmc = profile.characters.pmc;
 
-    const pmcInventory = pmc.Inventory;
-
-    const mainStashId = getMainStashId(profile);
-
-    if (mainStashId !== pmcInventory.stash) {
-      logger.success(
-        `=> PathToTarkov: restore the selected stash to main stash for profile '${profile.info.username}'`
-      );
-      pmcInventory.stash = mainStashId;
-    }
-
-    let nbTradersRestored = 0;
-    let jaegerLocked = false;
-    Object.keys(config.traders_config).forEach((traderId) => {
-      const pmc = profile.characters.pmc;
-      const trader = pmc.TradersInfo?.[traderId];
-      const jaegerAvailable = isJaegerIntroQuestCompleted(pmc.Quests);
-
-      if (
-        trader &&
-        trader.unlocked === false &&
-        ((traderId === JAEGER_ID && jaegerAvailable) || traderId !== JAEGER_ID)
-      ) {
-        trader.unlocked = true;
-        nbTradersRestored += 1;
-      } else if (
-        trader &&
-        trader.unlocked === true &&
-        traderId === JAEGER_ID &&
-        !jaegerAvailable
-      ) {
-        trader.unlocked = false;
-        jaegerLocked = true;
-      }
-    });
-
-    if (nbTradersRestored > 0) {
-      logger.success(
-        `=> PathToTarkov: ${nbTradersRestored} trader${
-          nbTradersRestored === 1 ? "" : "s"
-        } restored for profile '${profile.info.username}'`
-      );
-    }
-
-    if (jaegerLocked) {
-      logger.success(
-        `=> PathToTarkov: Jaeger trader locked (because introduction quest is not completed) for profile '${profile.info.username}'`
-      );
-    }
+    restoreMainStash(profile, logger);
+    restoreTraders(config, profile, logger);
   });
 
   saveServer.save();
