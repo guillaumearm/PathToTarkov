49fe5a43cdd93720ecb4937b2daeba988248ae55
Trap <contact@trapcodien.com>
2024-08-03 00:51:52 +0200

    chore(check-git-status): print error message at the end of the script

--- scripts/check-git-status.sh
+++ scripts/check-git-status.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
 if [[ `git status --porcelain` ]]; then
-    echo "Error from script 'check-git-status.sh' -> some changes are detected in this git repo"
     git status
+    echo "Error from script 'check-git-status.sh' -> some changes are detected in this git repo"
     exit 1
 else
     echo "OK: No changes detected."
