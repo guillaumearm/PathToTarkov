cd0f7bde984e6a9ef71e3f90181e234a11aa9882
Trap <contact@trapcodien.com>
2024-08-03 20:03:39 +0200

    build: fix prepare-files for windows usage

--- scripts/prepare-files.js
+++ scripts/prepare-files.js
@@ -2,25 +2,33 @@
 const { execSync } = require('child_process');
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const packageJson = require('../package.json');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { mkdirp } = require('mkdirp');
+// // eslint-disable-next-line @typescript-eslint/no-var-requires
+// const cpr = require('cpr');
 
-const modName = packageJson.fullName;
-
-const main = async () => {
+const main = async modName => {
   [
     'rm -rf dist/user',
     'rm -rf dist/BepInEx',
-    `mkdir -p ./dist/user/mods/${modName}`,
-    'mkdir -p ./dist/BepInEx/plugins',
-    'cp ./PTT-Extracts/bin/Debug/netstandard2.0/PTTExtracts.dll ./dist/BepInEx/plugins/',
-    `cp package.json ./dist/user/mods/${modName}`,
-    `cp -R dist/src ./dist/user/mods/${modName}`,
-    `cp -R config ./dist/user/mods/${modName}`,
-    `cp ALL_EXFILS.md ./dist/user/mods/${modName}`,
-    `cp LOGO.jpg ./dist/user/mods/${modName}`,
-    `cp README.md ./dist/user/mods/${modName}`,
-    `cp LICENSE ./dist/user/mods/${modName}`,
+    () => mkdirp.sync(`./dist/user/mods/${modName}`),
+    () => mkdirp.sync('./dist/BepInEx/plugins'),
+    'npx cpr ./PTT-Extracts/bin/Debug/netstandard2.0/PTTExtracts.dll ./dist/BepInEx/plugins/PTTExtracts.dll -o',
+    `npx cpr package.json ./dist/user/mods/${modName}/package.json -o`,
+    `npx cpr dist/src ./dist/user/mods/${modName} -o`,
+    `npx cpr config ./dist/user/mods/${modName} -o`,
+    `npx cpr ALL_EXFILS.md ./dist/user/mods/${modName}/ALL_EXFILS.md -o`,
+    `npx cpr LOGO.jpg ./dist/user/mods/${modName}/LOGO.jpg -o`,
+    `npx cpr README.md ./dist/user/mods/${modName}/README.md -o`,
+    `npx cpr LICENSE ./dist/user/mods/${modName}/LICENSE -o`,
     'echo "> Successfully prepared files!"',
-  ].forEach(cmd => process.stdout.write(execSync(cmd)));
+  ].forEach(cmd => {
+    if (typeof cmd === 'string') {
+      process.stdout.write(execSync(cmd));
+    } else {
+      cmd();
+    }
+  });
 };
 
-main();
+main(packageJson.fullName);
