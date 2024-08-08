5863529b9e3a971ee2353e3f58437615a482ed75
Trap <contact@trapcodien.com>
2024-08-08 15:56:38 +0200

    chore: add dev:install script in package.json

---
+++ scripts/install-files.js
@@ -0,0 +1,23 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { execSync } = require('child_process');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const packageJson = require('../package.json');
+
+const dllFileName = 'PTTExtracts.dll'
+const cprFlags = '--overwrite --delete-first'
+
+const main = async modName => {
+  [
+    `cpr ./dist/user/mods/${modName} ../../../user/mods/${modName} ${cprFlags}`,
+    `cpr ./dist/BepInEx/plugins/${dllFileName} ../../../BepInEx/plugins/${dllFileName} ${cprFlags}`,
+    'echo "> Successfully installed files!"',
+  ].forEach(cmd => {
+    if (typeof cmd === 'string') {
+      process.stdout.write(execSync(cmd));
+    } else {
+      cmd();
+    }
+  });
+};
+
+main(packageJson.fullName);
