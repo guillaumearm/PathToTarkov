48f3c0bb2404528f983ed587d2c5d8094496b075
devilflippy <julien-in-hell666@hotmail.fr>
2024-08-19 20:05:03 +0200

    Add Fr config to test

--- tests/configs.test.ts
+++ tests/configs.test.ts
@@ -68,4 +68,7 @@ describe('PTT embedded configs', () => {
   it('should validate the DevilFlippy config', () => {
     testConfig('./configs/DevilFlippy');
   });
+  it('should validate the DevilFlippy FR config', () => {
+    testConfig('./configs/DevilFlippy/FR');
+  });
 });
