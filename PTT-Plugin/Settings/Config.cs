f14992edb4843ab7bc90ecfa966153df3900ac40
Trap <trapcodien@gmail.com>
2024-12-29 21:54:53 +0100

    refactor: client Settings/Config class

--- PTT-Plugin/Settings/Config.cs
+++ PTT-Plugin/Settings/Config.cs
@@ -1,18 +1,20 @@
-using System.Collections.Generic;
 using BepInEx.Configuration;
 
 namespace PTT.Settings;
 
 internal class Config
 {
-    private static readonly List<ConfigEntryBase> ConfigEntries = [];
     private const string TradersSection = "Traders";
 
     internal static ConfigEntry<bool> ShowLockedTraders { get; set; }
 
     internal static void Init(ConfigFile config)
     {
-        ShowLockedTraders = config.Bind(TradersSection, "Show locked traders", false, "Please reload the traders page when you toggle this option.");
-        ConfigEntries.Add(ShowLockedTraders);
+        ShowLockedTraders = config.Bind(
+            TradersSection,
+            "Show locked traders",
+            false,
+            "Please reload the traders page when you toggle this option."
+        );
     }
 }
