a88d941864a44b33c9e80b8d319a7b8f997885f0
Trap <trapcodien@gmail.com>
2025-01-17 21:14:25 +0100

    build: images not packed in zip anymore

--- scripts/prepare-files.js
+++ scripts/prepare-files.js
@@ -11,25 +11,28 @@ const PTT_DLL_FILENAME = 'Trap.PathToTarkov.dll';
 const PTT_PACKETS_DLL_FILENAME = 'Trap.PathToTarkov-Packets.dll';
 
 const main = async modName => {
-  [
+  const serverModDir = `./dist/user/mods/${modName}`;
+
+  void [
     'rimraf dist/user',
     'rimraf dist/BepInEx',
-    () => mkdirp.sync(`./dist/user/mods/${modName}`),
+    () => mkdirp.sync(serverModDir),
     () => mkdirp.sync('./dist/BepInEx/plugins'),
     `cpr ./PTT-Plugin/bin/Debug/net471/${PTT_DLL_FILENAME} ./dist/BepInEx/plugins/${PTT_DLL_FILENAME} -o`,
     `cpr ./PTT-Packets/bin/Debug/net471/${PTT_PACKETS_DLL_FILENAME} ./dist/BepInEx/plugins/${PTT_PACKETS_DLL_FILENAME} -o`,
-    `cpr package.json ./dist/user/mods/${modName}/package.json -o`,
-    `cpr dist/src ./dist/user/mods/${modName}/src -o`,
-    `cpr configs ./dist/user/mods/${modName}/configs -o`,
-    `cpr ./dist/user/mods/${modName}/configs/shared_player_spawnpoints.json5 ./dist/user/mods/${modName}/src/do_not_distribute/shared_player_spawnpoints.json5 -o`,
-    `rimraf ./dist/user/mods/${modName}/configs/shared_player_spawnpoints.json5`,
-    `rimraf ./dist/user/mods/${modName}/configs/UserConfig.json5`,
-    `cpr ALL_EXFILS.md ./dist/user/mods/${modName}/ALL_EXFILS.md -o`,
-    `cpr LOGO.jpg ./dist/user/mods/${modName}/LOGO.jpg -o`,
-    `cpr README.txt ./dist/user/mods/${modName}/README.txt -o`,
-    // `cpr README.md ./dist/user/mods/${modName}/README.md -o`,
-    // `cpr ./docs ./dist/user/mods/${modName}/docs -o`,
-    `cpr LICENSE ./dist/user/mods/${modName}/LICENSE -o`,
+    `cpr package.json ${serverModDir}/package.json -o`,
+    `cpr dist/src ${serverModDir}/src -o`,
+    `cpr configs ${serverModDir}/configs -o`,
+    `rimraf ${serverModDir}/configs/**/*.jpg ${serverModDir}/configs/**/*.jpeg ${serverModDir}/configs/**/*.png ${serverModDir}/configs/**/*.gif`,
+    `cpr ${serverModDir}/configs/shared_player_spawnpoints.json5 ${serverModDir}/src/do_not_distribute/shared_player_spawnpoints.json5 -o`,
+    `rimraf ${serverModDir}/configs/shared_player_spawnpoints.json5`,
+    `rimraf ${serverModDir}/configs/UserConfig.json5`,
+    `cpr ALL_EXFILS.md ${serverModDir}/ALL_EXFILS.md -o`,
+    `cpr LOGO.jpg ${serverModDir}/LOGO.jpg -o`,
+    `cpr README.txt ${serverModDir}/README.txt -o`,
+    // `cpr README.md ${serverModDir}/README.md -o`,
+    // `cpr ./docs ${serverModDir}/docs -o`,
+    `cpr LICENSE ${serverModDir}/LICENSE -o`,
     'echo "> Successfully prepared files!"',
   ].forEach(cmd => {
     if (typeof cmd === 'string') {
