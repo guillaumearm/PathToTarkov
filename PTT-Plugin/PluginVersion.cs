eefa5e60bb1f0590b25d9eeffabab4ca751186fd
Trap <trapcodien@gmail.com>
2025-01-18 03:26:26 +0100

    6.0.0-rc.11

--- PTT-Plugin/PluginVersion.cs
+++ PTT-Plugin/PluginVersion.cs
@@ -6,5 +6,5 @@ namespace PTT;
 internal static class PluginVersion
 {
     internal const string VERSION = "6.0.0";
-    internal const string FULL_VERSION = "6.0.0-rc.10";
+    internal const string FULL_VERSION = "6.0.0-rc.11";
 }
